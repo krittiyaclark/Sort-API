e6a319e1f345e0bfd1b079ad7b2fd434337a7d71
krittiyaclark <krittiyaclark@gmail.com>
2019-07-08 11:57:23 -0400

    Successfully dispalyed both APIs result in the DOM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Card, CardTitle, CardText, Grid, Cell } from 'react-mdl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      workers: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    // const url = 'https://www.hatchways.io/api/assessment/work_orders';
+    fetch('https://www.hatchways.io/api/assessment/work_orders')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          loading: false,
+          items: json.orders
+        });
+        // console.log(this.state.items);
+        
+      });
+
+      fetch('https://www.hatchways.io/api/assessment/workers/0')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          loading: false,
+          workers: json.worker
+        });
+        // console.log(this.state.workers);
+
+      });
+  }
+
+  render() {
+    const { loading, items, workers } = this.state;
+  // let results = this.state.items.concat(this.state.workers)
+  // console.log(results)
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div style={{ width: '80%', margin: 'auto' }}>
+        <h1>Work Orders</h1>
+        <Grid className="demo-grid-1">
+          {items.map(item => (
+            <Cell col={4} key={item.id}>
+              <Card shadow={0} style={{ margin: 'auto' }}>
+                <CardTitle>
+                  Name : {item.name}
+                </CardTitle>
+                <CardText>
+                    <p><strong>Description:</strong> {item.description}</p>
+                    <p><strong>Deadline:</strong> {item.deadline}</p>
+                    <p><strong>CompanyName</strong> {workers.companyName}</p>
+                    <p><strong>Email</strong> {workers.email}</p>
+                    <p><strong>Image:</strong> {workers.image}</p>
+                    <p><strong>Name</strong> {workers.name}</p>
+                    </CardText>
+              </Card>
+            </Cell>
+          ))}
+          
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
